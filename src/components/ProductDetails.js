618ad8ce370928d02c6667a424f68d33ecf0963f
Vinicius Godinho Del Rio <vinicius.delrio41@gmail.com>
2023-08-12 22:59:03 -0300

    refactor: split some code of Products component into a new component ProductDetails

---
+++ src/components/ProductDetails.js
@@ -0,0 +1,22 @@
+let productCount = 0;
+
+function displayFormattedProductCount() {
+    return productCount > 0 ? productCount : 'Zero';
+}
+
+function ProductDetails(props) {
+    let badgeClass = 'badge-margin-left-240 badge ';
+    badgeClass += props.isAvailable ? "bg-success" : "bg-danger";
+    
+    return (
+        <div className="d-flex align-items-center justify-content-start mt-1">
+            <h6 className="font-weight-bold my-2" style={{'margin-right': 30}}>${props.price}</h6>
+            <button className="btn btn-primary">-</button>
+            <span style={{padding: '0px 14px', 'font-size': 13}}>{displayFormattedProductCount()}</span>
+            <button className="btn btn-primary">+</button>
+            <span className={badgeClass}>{props.isAvailable ? 'Available' : 'Unavailable' }</span>
+        </div>
+    );
+}
+
+export default ProductDetails;
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,32 +1,18 @@
-let productCount = 0;
-
-function displayFormattedProductCount() {
-    return productCount > 0 ? productCount : 'Zero';
-}
+import ProductDetails from "./ProductDetails";
 
 function Products(props) {
-    let badgeClass = 'badge-margin-left-240 badge ';
-    badgeClass += props.isAvailable ? "bg-success" : "bg-danger";
-
     return (
-        <ul className="list-group shadow">
-            <li className="list-group-item">
-                <div className="media align-items-lg-center flex-column flex-lg-row p-3 d-flex">
-                    <div className="media-body order-2 order-lg-1">
-                        <h5 className="mt-0 font-weight-bold mb-2">{props.name}</h5>
-                        <p className="font-italic text-muted mb-0 small">{props.description}</p>
-                        <div className="d-flex align-items-center justify-content-start mt-1">
-                            <h6 className="font-weight-bold my-2" style={{'margin-right': 30}}>${props.price}</h6>
-                            <button className="btn btn-primary">-</button>
-                            <span style={{padding: '0px 14px', 'font-size': 13}}>{displayFormattedProductCount()}</span>
-                            <button className="btn btn-primary">+</button>
-                            <span className={badgeClass}>{props.isAvailable ? 'Available' : 'Unavailable' }</span>
-                        </div>
-                    </div>
-                    <img src={props.imageUrl} width="200" className="ml-lg-5 order-1 order-lg-2" />
+        <li className="list-group-item">
+            <div className="media align-items-lg-center flex-column flex-lg-row p-3 d-flex">
+                <div className="media-body order-2 order-lg-1">
+                    <h5 className="mt-0 font-weight-bold mb-2">{props.name}</h5>
+                    <p className="font-italic text-muted mb-0 small">{props.description}</p>
+                    <ProductDetails price={props.price}
+                                    isAvailable={props.isAvailable} />
                 </div>
-            </li>
-        </ul>
+                <img src={props.imageUrl} width="200" className="ml-lg-5 order-1 order-lg-2" />
+            </div>
+        </li>
     );
 }
 

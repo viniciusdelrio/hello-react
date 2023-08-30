e67d2a700f1f2ab9c3fb4b2b8128ef2efa33c079
Vinicius Godinho Del Rio <vinicius.delrio41@gmail.com>
2023-08-30 13:09:55 -0300

    feat: dynamic product list

--- src/components/product-list/ProductList.js
+++ src/components/product-list/ProductList.js
@@ -51,41 +51,17 @@ function ProductList(props) {
         <div className='row'>
             <div className='col-lg-8 mx-auto'>
                 <ul className="list-group shadow">
-                    <Products 
-                        id={products[0].pID} 
-                        name={products[0].pName}
-                        description={products[0].desc}
-                        isAvailable={products[0].isAvailable}
-                        imageUrl={products[0].image}
-                        price={products[0].price} />
-                    <Products 
-                        id={products[1].pID} 
-                        name={products[1].pName}
-                        description={products[1].desc}
-                        isAvailable={products[1].isAvailable}
-                        imageUrl={products[1].image}
-                        price={products[1].price} />
-                    <Products 
-                        id={products[2].pID} 
-                        name={products[2].pName}
-                        description={products[2].desc}
-                        isAvailable={products[2].isAvailable}
-                        imageUrl={products[2].image}
-                        price={products[2].price} />
-                    <Products 
-                        id={products[3].pID} 
-                        name={products[3].pName}
-                        description={products[3].desc}
-                        isAvailable={products[3].isAvailable}
-                        imageUrl={products[3].image}
-                        price={products[3].price} />
-                    <Products 
-                        id={products[4].pID} 
-                        name={products[4].pName}
-                        description={products[4].desc}
-                        isAvailable={products[4].isAvailable}
-                        imageUrl={products[4].image}
-                        price={products[4].price} />
+                    {
+                        products.map((product) => { 
+                            return <Products 
+                                id={product.pID} 
+                                name={product.pName}
+                                description={product.desc}
+                                isAvailable={product.isAvailable}
+                                imageUrl={product.image}
+                                price={product.price}>
+                            </Products> })
+                    }
                 </ul>
             </div>
         </div>

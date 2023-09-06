81e50f4bfbe2037450abede98865b72c43e5b96f
Vinicius Godinho Del Rio <vinicius.delrio41@gmail.com>
2023-09-05 23:17:50 -0300

    feat: add created product to the productList

--- src/app.js
+++ src/app.js
@@ -3,18 +3,61 @@ import './app.css';
 import CreateProduct from './components/create-product/CreateProduct';
 import ProductList from './components/product-list/ProductList';
 
+let products = [
+    {
+        pID: 1, 
+        pName: 'Fresh Milk', 
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.',
+        isAvailable: true,
+        image: 'images/fresh-milk.png',
+        price: 12
+    },
+    {
+        pID: 2, 
+        pName: 'Cottage Cheese', 
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.',
+        isAvailable: false,
+        image: "images/cottage-cheese.png",
+        price: 10
+    },
+    {
+        pID: 3, 
+        pName: 'Brocoli', 
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.',
+        isAvailable: true,
+        image: "images/brocoli.png",
+        price: 15
+    },
+    {
+        pID: 4, 
+        pName: 'oranges', 
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.',
+        isAvailable: true,
+        image: "images/oranges.png",
+        price: 20
+    },
+    {
+        pID: 5, 
+        pName: 'Olive oil', 
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.',
+        isAvailable: false,
+        image: "images/olive-oil.png",
+        price: 14
+    }
+];
+
 function App() {
 
-    let [newProduct, updateProduct] = useState(null);
+    let [newProductList, updateProductList] = useState(products);
 
     function createProduct(product) {
-        updateProduct(product);
+        updateProductList([product, ...newProductList]);
     }
 
     return (
         <div>
             <CreateProduct createProduct={createProduct}></CreateProduct>
-            <ProductList newProduct={newProduct}></ProductList>
+            <ProductList newProductList={newProductList}></ProductList>
         </div>
     );
 }
--- src/components/product-list/ProductList.js
+++ src/components/product-list/ProductList.js
@@ -1,65 +1,19 @@
 import Products from './Products';
 
-let products = [
-    {
-        pID: 1, 
-        pName: 'Fresh Milk', 
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.',
-        isAvailable: true,
-        image: 'images/fresh-milk.png',
-        price: 12
-    },
-    {
-        pID: 2, 
-        pName: 'Cottage Cheese', 
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.',
-        isAvailable: false,
-        image: "images/cottage-cheese.png",
-        price: 10
-    },
-    {
-        pID: 3, 
-        pName: 'Brocoli', 
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.',
-        isAvailable: true,
-        image: "images/brocoli.png",
-        price: 15
-    },
-    {
-        pID: 4, 
-        pName: 'oranges', 
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.',
-        isAvailable: true,
-        image: "images/oranges.png",
-        price: 20
-    },
-    {
-        pID: 5, 
-        pName: 'Olive oil', 
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.',
-        isAvailable: false,
-        image: "images/olive-oil.png",
-        price: 14
-    }
-];
-
 function ProductList(props) {
-
-    console.log(props.newProduct);
-
     return (
         <div className='row'>
             <div className='col-lg-8 mx-auto'>
                 <ul className="list-group shadow">
                     {
-                        products.map((product) => { 
+                        props.newProductList.map((product) => { 
                             return <Products 
-                                id={product.pID} 
-                                name={product.pName}
-                                description={product.desc}
-                                isAvailable={product.isAvailable}
-                                imageUrl={product.image}
-                                price={product.price}>
+                                id={product?.pID} 
+                                name={product?.pName}
+                                description={product?.desc}
+                                isAvailable={product?.isAvailable}
+                                imageUrl={product?.image}
+                                price={product?.price}>
                             </Products> })
                     }
                 </ul>

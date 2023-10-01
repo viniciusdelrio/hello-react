8650430b81b2c62af9aee6d49ddad885d6d5e1a6
Vinicius Godinho Del Rio <vinicius.delrio41@gmail.com>
2023-10-01 18:55:03 -0300

    feat: render conditional content on ProductList

--- src/Components/ProductList/ProductList.js
+++ src/Components/ProductList/ProductList.js
@@ -1,22 +1,24 @@
 import Products from './Products';
 
 function ProductList(props) {
-    return (
-            <ul className="list-group shadow">
-                {
-                    props.newProductList.map((product) => { 
-                        return <Products 
-                            key={product?.pID}
-                            id={product?.pID} 
-                            name={product?.pName}
-                            description={product?.desc}
-                            isAvailable={product?.isAvailable}
-                            imageUrl={product?.image}
-                            price={product?.price}>
-                        </Products> })
-                }
-            </ul>
-    );
+    return props.newProductList.length === 0
+        ? <h3>No products available!</h3>
+        :  (
+                <ul className="list-group shadow">
+                    {
+                        props.newProductList.map((product) => { 
+                            return <Products 
+                                key={product?.pID}
+                                id={product?.pID} 
+                                name={product?.pName}
+                                description={product?.desc}
+                                isAvailable={product?.isAvailable}
+                                imageUrl={product?.image}
+                                price={product?.price}>
+                            </Products> })
+                    }
+                </ul>
+            );
 }
 
 export default ProductList;
